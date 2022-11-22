fe1319ff740ee5284186f7adcf3835462f069a83
Simeon Lukas <rtdust@gmail.com>
2022-11-22 07:11:17 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,24 @@ fn main() {
                                                             ^~~~!.           .7~~~~~:          :~~^!
                                                             ~^~~~:         ^^^^^::::^^^.      ^~~~^~
 Aufbereitungstool für die ImpulsAO-APP und deren Server.
+Zur Aufbereitung benötigst du alle Impulse in einem Ordner.
+
+Bitte benenne den Ordner je nach Jahr (Beispiel: 2022) und der Kalendernummer des Jahres (01 Fastenzeit bzw. 02 Advent).
+Der Ordner für die Impulse der Fastenzeit 2025 würde folgenden Namen tragen: 202501
+Der Ordner für die Impulse der Adventszeit 2024 würde folgenden Namen tragen: 202402
+
+In dem Ordner sollen nur die Bilder der Impulse liegen. Diese Bilder sind je nach Datum benannt.
+Hier ein Beispiel:
+Der Impuls für den 1. Dezember 2024 trägt den Namen 20241201.jpg
+Der Impuls für den 2. Januar 2025 trägt den Namen 20250102.jpg
+
+Das Foto der Autoren sollte dann folgenden Titel tragen: 'Ordnername'-team.jpg
+Das Foto der Autoren vom Adventskalender 2024 würde folgenden Namen tragen: 202402-team.jpg
+
+Die Impuls_ao_Creator.exe muss im gleichen Ordner liegen wie der Ordner mit den Impulsen.
+
+Alles ❤ ,
+Simeon
 "
     );
     converter();
@@ -67,8 +85,7 @@ fn converter() {
     let mut vec = Vec::new();
     let mut vecextra = Vec::new();
     println!(
-        "In welchem Ordner liegen die Impulse?
-Format: JJJJMMDD.jpg --> 01. Dezember 2025 = 20251201.jpg"
+        "In welchem Ordner liegen die Impulse?"
     );
     io::stdin()
         .read_line(&mut dir)
